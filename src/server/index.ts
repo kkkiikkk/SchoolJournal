286e105664e27577ac08c12d6738d8103adfc935
user01 <tedpoweer@gmail.com>
2022-01-17 17:23:40 +0700

    upgrade

--- src/server/index.ts
+++ src/server/index.ts
@@ -10,10 +10,9 @@ import * as HapiPulse from 'hapi-pulse';
 import * as Qs from 'qs';
 import routes from './routes';
 import config from './config/config';
-import { handleValidationError, responseHandler, } from './utils';
-import { tokenValidate, } from './utils/auth';
+import {handleValidationError, responseHandler,} from './utils';
 import SwaggerOptions from './config/swagger';
-import { pinoConfig, } from './config/pino';
+import {pinoConfig,} from './config/pino';
 
 const HapiSwagger = require('hapi-swagger');
 const Package = require('../../package.json');
@@ -21,98 +20,63 @@ const Package = require('../../package.json');
 SwaggerOptions.info.version = Package.version;
 
 const init = async () => {
-  const server = await new Hapi.Server({
-    port: config.server.port,
-    host: config.server.host,
-    query: {
-      parser: (query) => Qs.parse(query),
-    },
-    routes: {
-      validate: {
-        options: {
-          // Handle all validation errors
-          abortEarly: false,
+    const server = await new Hapi.Server({
+        port: config.server.port,
+        host: config.server.host,
+        query: {
+            parser: (query) => Qs.parse(query),
         },
-        failAction: handleValidationError,
-      },
-      response: {
-        failAction: 'log',
-      },
-    },
-  });
-  server.realm.modifiers.route.prefix = '/api';
-  // Регистрируем расширения
-  await server.register([
-    Basic,
-    Nes,
-    Inert,
-    Vision,
-    HapiBearer,
-    { plugin: Pino, options: pinoConfig(false), },
-    { plugin: HapiSwagger, options: SwaggerOptions, }
-  ]);
+        routes: {
+            validate: {
+                options: {
+                    // Handle all validation errors
+                    abortEarly: false,
+                },
+                failAction: handleValidationError,
+            },
+            response: {
+                failAction: 'log',
+            },
+        },
+    });
+    server.realm.modifiers.route.prefix = '/api';
+    // Регистрируем расширения
+    await server.register([
+        Basic,
+        Nes,
+        Inert,
+        Vision,
+        HapiBearer,
+        {plugin: Pino, options: pinoConfig(false),},
+        {plugin: HapiSwagger, options: SwaggerOptions,},
+        {
+            plugin: HapiPulse,
+            options: {
+                timeout: 15000,
+                signals: ['SIGINT'],
+            },
+        },
+        {
+            plugin: HapiCors,
+            options: config.cors,
+        }
+    ]);
 
-  // Авторизация через соцсети
-  // server.auth.strategy('google', 'bell', {
-  //   provider: 'Google',
-  //   clientId: process.env.auth_google_id,
-  //   clientSecret: process.env.auth_google_secret,
-  //   password: process.env.auth_google_secret,
-  //   isSecure: false
-  // });
-  // server.auth.strategy('fb', 'bell', {
-  //   provider: 'Facebook',
-  //   clientId: Number(process.env.auth_fb_id),
-  //   clientSecret: process.env.auth_fb_secret,
-  //   password: process.env.auth_fb_cookie_password,
-  //   isSecure: false
-  // });
-  // server.auth.strategy('vk', 'bell', {
-  //   provider: 'VK',
-  //   clientId: Number(process.env.auth_vk_id),
-  //   clientSecret: process.env.auth_vk_secret,
-  //   password: process.env.auth_vk_cookie_password,
-  //   isSecure: false
-  // });
 
-  // Авторизация стандартная (логин+пароль)
-  // server.auth.strategy('simple', 'basic', { validate: basicAuthHandler });
+    // Загружаем маршруты
+    server.route(routes);
+    // Error handler
+    // server.ext('onPreResponse', responseHandler);
+    // Enable CORS (Do it last required!)
+    // Запускаем сервер
+    try {
+        await server.start();
+        server.log('info', `Server running at: ${server.info.uri}`);
+    } catch (err) {
+        server.log('error', JSON.stringify(err));
+    }
 
-  // JWT Auth
-  server.auth.strategy('jwt-access', 'bearer-access-token', {
-    validate: tokenValidate('access'),
-  });
-  server.auth.strategy('jwt-refresh', 'bearer-access-token', {
-    validate: tokenValidate('refresh'),
-  });
-  server.auth.default('jwt-access');
-
-  // Загружаем маршруты
-  server.route(routes);
-  // Error handler
-  server.ext('onPreResponse', responseHandler);
-  await server.register({
-    plugin: HapiPulse,
-    options: {
-      timeout: 15000,
-      signals: ['SIGINT'],
-    },
-  });
-  // Enable CORS (Do it last required!)
-  await server.register({
-    plugin: HapiCors,
-    options: config.cors,
-  });
-  // Запускаем сервер
-  try {
-    await server.start();
-    server.log('info', `Server running at: ${server.info.uri}`);
-  }
-  catch (err) {
-    server.log('error', JSON.stringify(err));
-  }
-
-  return server;
+    return server;
 };
 
-export { init, };
+export {init,};
