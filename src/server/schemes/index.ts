a93d680b717cd1dd3edc4c0a04696583fd7c6928
kkkiikkk <88326234+kkkiikkk@users.noreply.github.com>
2022-01-25 08:03:24 +0200

    feat: add register and auth user

--- src/server/schemes/index.ts
+++ src/server/schemes/index.ts
@@ -45,21 +45,6 @@ export const userLogin = Joi.object({
     .required()  
 })
 
-export const userUpdate = Joi.object({
-  username: Joi.string(),
-  password: Joi.string()
-    .min(6)
-    .max(24)
-    .required(),
-  phone: Joi.string()
-    .regex(/\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/)
-    .required(),
-  dateOfBirth: Joi.date()
-    .raw()
-    .required(),
-  sex: Joi.string()
-    .valid('male', 'female')
-    .required()
-})
+
 
 export { user };
